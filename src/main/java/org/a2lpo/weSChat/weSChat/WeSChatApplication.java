e4d0669eb36b184a2a6f4320be23baf660189d7a
Andrey <popov.a83@bk.ru>
2018-09-19 21:05:02 +0200

    first commit

---
+++ src/main/java/org/a2lpo/weSChat/weSChat/WeSChatApplication.java
@@ -0,0 +1,12 @@
+package org.a2lpo.weSChat.weSChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeSChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeSChatApplication.class, args);
+	}
+}
---
+++ src/test/java/org/a2lpo/weSChat/weSChat/WeSChatApplicationTests.java
@@ -0,0 +1,16 @@
+package org.a2lpo.weSChat.weSChat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WeSChatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
